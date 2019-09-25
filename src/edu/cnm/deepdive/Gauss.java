ef5755cd03995e9df72dfdcd48ad96b14c4e505a
finn <isaaclindland@protonmail.com>
2019-09-25 09:25:55 -0600

    refactored to allow functions to accept and return longs

--- src/edu/cnm/deepdive/Gauss.java
+++ src/edu/cnm/deepdive/Gauss.java
@@ -1,31 +1,37 @@
 package edu.cnm.deepdive;
 
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class Gauss {
 
-  private static Integer UPPER_BOUND;
+  private final static Long UPPER_BOUND = 100L;
 
   public static void main(String[] args) {
 
-    UPPER_BOUND = (args.length > 0 ? Integer.parseInt(args[0]):100);
-    System.out.println(computeNaiveSumLoop(UPPER_BOUND));
-    System.out.println(computeNaiveSumStream(UPPER_BOUND));
-    System.out.println(computeFunction(UPPER_BOUND));
+    Long upperBound;
+    try {
+      upperBound = Long.parseLong(args[0]);
+    } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+      upperBound = UPPER_BOUND;
+    }
+
+    System.out.println(computeNaiveSumStream(upperBound));
+    System.out.println(computeNaiveSumLoop(upperBound));
+    System.out.println(computeFunction(upperBound));
   }
 
-  static Integer computeNaiveSumStream(Integer upperBound){
-    return IntStream.range(1, UPPER_BOUND +1).sum();
+  static Long computeNaiveSumStream(Long upperBound){
+    return LongStream.range(1, upperBound + 1).sum();
   }
-  static Integer computeNaiveSumLoop(Integer upperBound){
-    Integer sum = 0;
-    for (int i = 0; i < upperBound +1 ; i++) {
+  static Long computeNaiveSumLoop(Long upperBound){
+    Long sum = 0L;
+    for (Integer i = 0; i < upperBound +1 ; i++) {
       sum+=i;
     }
     return sum;
   }
-  static Integer computeFunction(Integer upperBound){
-    return (int) (Math.pow(upperBound, 2) + upperBound) / 2;
+  static Long computeFunction(Long upperBound){
+    return ((long) (Math.pow(upperBound, 2) + upperBound) / 2);
   }
 
 
